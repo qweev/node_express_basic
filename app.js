a65cf2f8c3737b9e0e677e69104655db71102f8f
migdal <wojciech.migdal@nokia.com>
2021-12-01 13:46:39 +0100

    validator added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var router = require("./extapi/extapi.js");
 var vv = require("./vars.js");
 var car = require("./carClass.js");
+var validator = require("validator");
 
 var c = new car("abc", 123);
 console.log("car message:" + c.age());
@@ -40,9 +41,17 @@ app.get("/api", (req, res) => {
   res.send("param " + p1 + p2);
 });
 
+//validator example
+//http://localhost:3003/val/22
+app.get("/val/:val", (req, res) => {
+  console.log("val request");
+  var p1 = req.params.val;
+  console.log("is num:" + validator.isNumeric(p1));
+  console.log("is IP:" + validator.isIP(p1));
+  var r = validator.isNumeric(p1);
+  res.send("val = " + r);
+});
+
 app.listen(3003, function () {
   console.log("server run");
 });
-
-//todo add validation for query params
-// add linter check
